66f6be4d5c3c839a67135bfea303c6c4aea5d901
Alexander Qi <xdqi@users.noreply.github.com>
2018-02-21 14:17:37 +0800

    Fix tabs

--- so/libipt_ICMPD.c
+++ so/libipt_ICMPD.c
@@ -12,7 +12,7 @@ enum {
 	O_IFADDR,
 	F_TO        = 1 << O_TO,
 	F_IFADDR    = 1 << O_IFADDR,
-    F_ANY       = F_TO | O_IFADDR,
+	F_ANY       = F_TO | O_IFADDR,
 };
 
 static void ICMPD_help(void)
@@ -35,25 +35,25 @@ static const struct xt_option_entry ICMPD_opts[] = {
 static void ICMPD_parse(struct xt_option_call *cb)
 {
 	struct ipt_ICMPD_info *info = cb->data;
-    const struct in_addr *ip = NULL;
+	const struct in_addr *ip = NULL;
 
 	xtables_option_parse(cb);
 	switch (cb->entry->id) {
 	case O_TO:
 		info->mode = IPT_ICMPD_TO;
-        ip = xtables_numeric_to_ipaddr(cb->arg);
-        if (!ip) {
-            xtables_error(PARAMETER_PROBLEM, "Bad IP address \"%s\"\n", cb->arg);
-            return;
-        }
-        info->target.addr.ip = ip->s_addr;
+		ip = xtables_numeric_to_ipaddr(cb->arg);
+		if (!ip) {
+			xtables_error(PARAMETER_PROBLEM, "Bad IP address \"%s\"\n", cb->arg);
+			return;
+		}
+		info->target.addr.ip = ip->s_addr;
 		break;
 	case O_IFADDR:
 		info->mode = IPT_ICMPD_IFADDR;
-        memcpy(&info->target.ifname, cb->arg, IFNAMSIZ);
+		memcpy(&info->target.ifname, cb->arg, IFNAMSIZ);
 		break;
-    default:
-        printf("Unknown mode\n");
+	default:
+		printf("Unknown mode\n");
 	}
 }
 
@@ -64,22 +64,22 @@ static void ICMPD_fcheck(struct xt_fcheck_call *cb)
 }
 
 static void ICMPD_print(const void *ip, const struct xt_entry_target *target,
-                        int numeric)
+						int numeric)
 {
 	const struct ipt_ICMPD_info *info = (const void *)target->data;
 
-    struct in_addr a;
+	struct in_addr a;
 
 	printf(" to:");
-    switch (info->mode) {
-    case IPT_ICMPD_TO:
-        a = info->target.addr.in;
-        printf(" %s", xtables_ipaddr_to_numeric(&a));
-        break;
-    case IPT_ICMPD_IFADDR:
-        printf(" address of interface %s", info->target.ifname);
-        break;
-    }
+	switch (info->mode) {
+	case IPT_ICMPD_TO:
+		a = info->target.addr.in;
+		printf(" %s", xtables_ipaddr_to_numeric(&a));
+		break;
+	case IPT_ICMPD_IFADDR:
+		printf(" address of interface %s", info->target.ifname);
+		break;
+	}
 }
 
 static void ICMPD_save(const void *ip, const struct xt_entry_target *target)
@@ -87,17 +87,17 @@ static void ICMPD_save(const void *ip, const struct xt_entry_target *target)
 	const struct ipt_ICMPD_info *info = (const void *)target->data;
 	unsigned int i = 0;
 
-    struct in_addr a;
+	struct in_addr a;
 
-    switch (info->mode) {
-    case IPT_ICMPD_TO:
-        a.s_addr = info->target.addr.ip;
-        printf(" --to %s", xtables_ipaddr_to_numeric(&a));
-        break;
-    case IPT_ICMPD_IFADDR:
-        printf(" --ifaddr %s", info->target.ifname);
-        break;
-    }
+	switch (info->mode) {
+	case IPT_ICMPD_TO:
+		a.s_addr = info->target.addr.ip;
+		printf(" --to %s", xtables_ipaddr_to_numeric(&a));
+		break;
+	case IPT_ICMPD_IFADDR:
+		printf(" --ifaddr %s", info->target.ifname);
+		break;
+	}
 }
 
 static struct xtables_target icmpd_tg_reg = {
